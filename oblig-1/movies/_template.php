a61cabd0f5afbe7eab11cfc2a1ab36d12774dc27
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-01-29 16:48:12 +0100

    Updated movie template

--- oblig-1/movies/_template.php
+++ oblig-1/movies/_template.php
@@ -1,13 +1,17 @@
-<main>
-  <article id="movie-summary-<?= $movie['slug']; ?>">
-    <img src="<?= $_path; ?>images/movies/<?= $movie['slug']; ?>.jpg" alt="<?= $movie['name']; ?> movie poster">
-    <h2><?= $movie['name']; ?></h2>
-    <p><?= $movie['description']; ?></p>
-    <h3>Cast</h3>
-    <?php
-      foreach ($movie['cast'] as $role => $person) {
-        echo '<p><strong>' . ucfirst($role) . ':</strong> ' . $person['name_real'] . ' as ' . $person['name_role'] . '</p>';
-      }
-    ?>
+<main id="movie-<?= $movie['slug']; ?>" class="page-movie">
+  <article>
+    <img class="movie-poster" src="<?= $_path; ?>images/movies/<?= $movie['slug']; ?>.jpg" alt="<?= $movie['name']; ?> movie poster">
+    <div class="movie-info">
+      <h1><?= $movie['name']; ?>.</h2>
+      <p><?= $movie['description']; ?></p>
+      <div class="button-wrapper">
+        <?php
+          foreach ($movie['cast'] as $role => $person) {
+            echo '<p><strong>' . ucfirst($role) . ':</strong> ' . $person['name_real'] . ' as ' . $person['name_role'] . '</p>';
+          }
+        ?>
+        <a href="<?= $movie['links']['imdb'] ?>" class="button" target="_blank" rel="noreferrer nofollow noopener">Les mer på IMDB</a>
+      </div>
+    </div>
   </article>
 </main>
