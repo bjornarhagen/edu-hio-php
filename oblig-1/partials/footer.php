ce652fb5e46bed721f879e79fb4c197eb8a846f1
Bjørnar Hagen <bjornar@datahjelpen.no>
2019-02-09 15:27:34 +0100

    Added footer

--- oblig-1/partials/footer.php
+++ oblig-1/partials/footer.php
@@ -1,4 +1,8 @@
-<link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Playfair+Display:700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700" rel="stylesheet">
+    <footer>
+        <p>CC BY 4.0 – Bjørnar Hagen</p>
+        <p>Source: <a href="https://github.com/bjornarhagen/edu-hio-php/">github.com/bjornarhagen/edu-hio-php</a></p>
+    </footer>
 </body>
 </html>
